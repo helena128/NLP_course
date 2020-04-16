cdc63e20de7d53f419e6cb946e5f16d257f6f558
Elena Tcheprasova <hellogbel@gmail.com>
2020-04-16 16:17:19 +0300

    Fixed lab 2

--- task2/script.py
+++ task2/script.py
@@ -3,8 +3,10 @@ from numpy import array_split
 
 file_path = "/home/helena/Documents/NLP/data/study_in_scarlet.txt"
 num_of_sentences = 30
-'''
-def build_positional_index(positional_index_dict, documents):
+
+# Pos index structure: {word: {doc: [po1, pos2]}}
+def build_positional_index(documents):
+	positional_index_dict = {}
 	for doc_id in range(len(documents)):
 		for word_id, word in enumerate(word_tokenize(documents[doc_id])):
 			if str(word) not in positional_index_dict:
@@ -12,25 +14,34 @@ def build_positional_index(positional_index_dict, documents):
 			if doc_id not in positional_index_dict[word]:
 				positional_index_dict[word][doc_id] = []
 			positional_index_dict[word][doc_id].append(word_id)
-'''
-'''
-def search(search_query, positional_index_dict):
-	print('Searching for: ', search_query)
+	return positional_index_dict
+
+# search function
+def search(search_query, documents, positional_index_dict):
+	#print('Searching for: ', search_query)
 	search_query_tokens = word_tokenize(str(search_query))
+
+	for token in search_query_tokens:
+		if token not in positional_index_dict:
+			print('Token <', token, '> was not found in dict')
+			return []
+
 	result_docs = []
-	res_for_curr_doc = {}
-	for doc_id in range(len(positional_index_dict)):
-		for token_id in range(len(search_query_tokens)):
-			token = search_query_tokens[token_id]
-			if search_query_tokens[token] in positional_index_dict:
-				res_for_curr_doc[token] = search_query_tokens[token] 
-			else: break
-			if token_id > 0 and bool(res_for_curr_doc[search_query_tokens[token_id - 1]]):
-				break
-			if token_id == len(search_query_tokens) - 1:
-				res_for_curr_doc.append(doc_id)
-	print('Doc Ids: ', result_docs)
-'''
+	first_token = search_query_tokens[0]
+
+	for doc_id in range(len(documents)):
+		# If first token exists in positional index for current doc
+		if doc_id in positional_index_dict[first_token]:
+			for ft_pos in positional_index_dict[first_token][doc_id]:
+				flag = True
+				for inc, token in enumerate(search_query_tokens[1:], start=1):
+					#print(doc_id, ' ', ft_pos, inc, token)
+					if doc_id not in positional_index_dict[token] or (ft_pos + inc) not in positional_index_dict[token][doc_id]:
+						flag = False
+						break
+				if flag:
+					result_docs.append((doc_id, ft_pos))
+	return result_docs
 
 # Read file
 file = open(file_path, 'r')
@@ -38,30 +49,17 @@ text = file.read()
 
 # Tokenize into sentences
 sentences = sent_tokenize(text)
-print(type(sentences))
+#print(type(sentences), sentences[:3])
 
 # Split - create docs
-documents = []
-curr_doc = []
-for sent_id in range(1, len(sentences)):
-	if sent_id % num_of_sentences == 1:
-		curr_doc = []
-		continue
-	if sent_id % num_of_sentences == 0:
-		documents.append(''.join(curr_doc))
-		continue
-	else:
-		curr_sent = sentences[sent_id]
-		curr_doc.append[curr_sent]
+documents = ['\n'.join(sentences[x: x + num_of_sentences]) for x in range(0, len(sentences), num_of_sentences)]
 
-print('Num of docs: ', len(documents))
+#print('Num of docs: ', len(documents))
+#print('1st doc: ', documents[0])
 
 # Create positional index
-positional_index_dict = {}
-#build_positional_index(positional_index_dict, documents)
+positional_index_dict = build_positional_index(documents)
 
 # Search
-'''
-search('STUDY IN', positional_index_dict)
-search('No res', positional_index_dict)
-'''
+print(search('Indian possessions', documents, positional_index_dict))
+print(search('No res', documents, positional_index_dict))
